cceb28ed4f5a3269062ffa3ee7795db23dd42e44
Atsumi3 <atsumi@mail.bizen.jp>
2022-02-07 17:17:38 +0900

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "helloKMM",
-            url: "https://github.com/Atsumi3/helloKMM/archive/refs/tags/1.0.0.zip"
+            url: "https://github.com/Atsumi3/helloKMM/releases/download/1.0.0/helloKMM.xcframework.zip"
+            checksum: "bc427bac7b61b45b30968880421e38b81eafb9df82c3b36df0d1c25e7fe2b08a"
         )
     ]
 )
